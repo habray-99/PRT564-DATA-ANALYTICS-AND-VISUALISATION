3a558c56a676006259854f0bf15d0b1b652841a0
bishalbhujel <bishalbhujel112@gmail.com>
2026-04-15 22:13:48 +1000

    Changes made to read multiple sheets from single file

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ dataset_folder = root_folder + "/dataset"
 for filename in os.listdir(dataset_folder):
     file_path = dataset_folder + "/" + filename
     print(file_path)
-    print(f"Opening file {filename}")
-    print("------------------------------------------------------")
-    df = pd.read_excel(file_path, skiprows= 1)
-    print(df)
-    print("------------------------------------------------------")
-    print(f"closinf file {filename}")
+    try:
+        print(f"Opening file {filename}")
+        ind_file = pd.ExcelFile(file_path)
+        print("------------------------------------------------------")
+        sheets_to_process = [sheet for sheet in ind_file.sheet_names if sheet.startswith("Data")]
+        print(sheets_to_process)
+        df = pd.read_excel(file_path, skiprows= 1)
+        print(df)
+        print("------------------------------------------------------")
+        print(f"closing file {filename}")
+    except Exception as e:
+        print('file cannot be found')
